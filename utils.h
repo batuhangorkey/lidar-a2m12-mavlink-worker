a26e7aa53e676ada366863c43fde46489f251755
Batuhan GÖrkey <ari.batuhangorkey@gmail.com>
2025-04-25 11:19:27 +0300

    Remove description comment from utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,3 @@
-// Description: Utility functions and macros for C++ projects
-
 #define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
-
 #define sleep_s(x) std::this_thread::sleep_for(std::chrono::seconds(x))
 #define sleep_ms(x) std::this_thread::sleep_for(std::chrono::milliseconds(x))
